b61168d76ef3bf10e12ae933141ce89d38eb2657
siva741 <chsivasai630111@gmail.com>
2026-06-14 18:09:34 +0530

    chore: configure dns servers in db config and add mongodb-memory-server support for local development

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const isMongoUri = (uri = '') => uri.startsWith('mongodb://') || uri.startsWith('mongodb+srv://');
 
